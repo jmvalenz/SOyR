265b7643f2b4ea9d3c1333b47cfa7dd1787607de
Felipe Sagredo <fnsagred@gmail.com>
2013-10-20 00:09:22 -0300

    writeToDisc listo

--- Tarea 2/DiscController.py
+++ Tarea 2/DiscController.py
@@ -2,9 +2,37 @@ from boot import*
 
 def readFromDisc(block): 
 	f = open('disco/'+str(block)+'.txt')
+	f.readline()
 	info=f.readlines()
 	f.close()
 	info=[item.strip() for item in info]
 	return info
 
-boot()
+      
+def writeToDisc(info, tipo):##tipo se refiere a si se esta escribiendo un contacto, mensaje o historial
+        boot()
+        for i in range(0,len(availableDisc)):
+                boot()
+                u=availableDisc[i][:3]
+                w=open('disco/'+str(int(u))+'.txt','a')
+                r=open('disco/'+str(int(u))+'.txt','r')
+                tipoArchivo=r.readline()
+                tipoArchivo=tipoArchivo[:-1]
+                if tipoArchivo=="":
+                        w.write(tipo+"\n")
+                        w.write(info+"\n")
+                        return
+                elif tipoArchivo==tipo:
+                        l=r.readlines()
+                        cont=0
+                        for j in range(0,len(l)):
+                                cont=cont+len(l[j][:-1])
+                        espacioUsado=len(tipoArchivo)+cont
+                        if espacioUsado<=513:
+                                w.write(info+"\n")
+                                return
+
+writeToDisc("hola","mensaje")
+                                
+                
+     
